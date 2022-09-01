fee2e6a8c5344e37a3638adb65ef26c849677401
yrom1 <ryanm.inbox@gmail.com>
2022-09-01 19:34:52 -0400

    Before deleting cat images

---
+++ find_bad_cats.py
@@ -0,0 +1,10 @@
+with open("bad-cats.txt", "r") as f:
+    content = f.read()
+
+import re
+import os
+
+for file in re.findall(r"[a-zA-Z]*\/[0-9]*\.jpg", content):
+    delete_me = str(os.path.join("/Users/ryan/data/", file))
+    print(delete_me)
+    # os.remove(delete_me)
