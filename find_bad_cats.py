63fdd32fb7e5bb128667006788cd62a5dacafe18
yrom1 <ryanm.inbox@gmail.com>
2022-09-01 19:38:39 -0400

    Consider the cats gone

--- find_bad_cats.py
+++ find_bad_cats.py
@@ -6,5 +6,8 @@ import os
 
 for file in re.findall(r"[a-zA-Z]*\/[0-9]*\.jpg", content):
     delete_me = str(os.path.join("/Users/ryan/data/", file))
-    print(delete_me)
-    # os.remove(delete_me)
+    print(delete_me, os.path.isfile(delete_me))
+    try:
+        os.remove(delete_me)
+    except:
+        print("exception for", delete_me)
